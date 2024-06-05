ba825abc216247b8b4dbbbb8087f67669f914242
André Luiz <andre.doctors@gmail.com>
2024-06-05 00:46:23 -0400

    first commit

---
+++ src/main/java/br/com/api/cadastroCliente/CadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.cadastroCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClienteApplication.class, args);
+	}
+
+}
